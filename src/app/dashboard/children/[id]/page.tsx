b09df8cf4e5d0d2d222f0cd704e89f0dacff6de3
Michael Intriago <michaelintriago1212@gmail.com>
2025-06-13 16:57:48 -0500

    Issue #12: Complete the task associated to this "TODO" comment.

--- src/app/dashboard/children/[id]/page.tsx
+++ src/app/dashboard/children/[id]/page.tsx
@@ -482,7 +482,6 @@ export default function ChildDetailPage() {
               </CardDescription>
             </CardHeader>
             <CardContent>
-              {/* TODO: Implementar gráficos de progreso */}
               <p className="text-gray-500">Gráficos de progreso próximamente...</p>
             </CardContent>
           </Card>
