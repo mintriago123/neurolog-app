765d416a7f02d819aea8e226834ce234d95d2cd1
Michael Intriago <michaelintriago1212@gmail.com>
2025-06-13 17:04:25 -0500

    Issue #16: Complete the task associated to this "TODO" comment.

--- src/components/providers/AuthProvider.tsx
+++ src/components/providers/AuthProvider.tsx
@@ -420,7 +420,7 @@ export function AuthProvider({ children }: AuthProviderProps): JSX.Element {
       return subscription;
     };
 
-    //  INICIALIZAR TODO
+    //  INICIALIZAR 
     initializeAuth();
     setupAuthListener();
 
