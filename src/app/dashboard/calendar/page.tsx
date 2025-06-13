ac097da4b99d1702d50df0c92d085304ce56fae1
Michael Intriago <michaelintriago1212@gmail.com>
2025-06-13 16:45:22 -0500

    Issue#9: Make sure that using this pseudorandom number generator is safe here.

--- src/app/dashboard/calendar/page.tsx
+++ src/app/dashboard/calendar/page.tsx
@@ -115,6 +115,7 @@ export default function CalendarPage() {
               >
                 {format(day, 'd')}
                 {/* Placeholder for events */}
+                {/* Uso no crítico de Math.random() para efectos visuales - seguro en este contexto */}
                 {Math.random() > 0.8 && (
                   <div className="absolute bottom-1 left-1/2 transform -translate-x-1/2 w-1 h-1 bg-blue-500 rounded-full"></div>
                 )}
