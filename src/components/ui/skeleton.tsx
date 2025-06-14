f4bc2ade08663c1941d5fc278569d88d695f0ec0
Michael Intriago <michaelintriago1212@gmail.com>
2025-06-13 21:12:22 -0500

    Issue #231...#232

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils"
 function Skeleton({
   className,
   ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
+}: Readonly< React.HTMLAttributes<HTMLDivElement>>) {
   return (
     <div
       className={cn(
@@ -66,15 +66,18 @@ function SkeletonAvatar() {
 function SkeletonText({ lines = 3 }:  Readonly<{ lines?: number }>) {
   return (
     <div className="space-y-2">
-      {Array.from({ length: lines }).map((_, i) => (
-        <Skeleton 
-          key={i} 
-          className={cn(
-            "h-4",
-            i === lines - 1 ? "w-[80%]" : "w-full"
-          )} 
-        />
-      ))}
+      {Array.from({ length: lines }).map((_, i) => {
+        const key = `skeleton-text-line-${lines}-${i}`;
+        return (
+          <Skeleton 
+            key={key} 
+            className={cn(
+              "h-4",
+              i === lines - 1 ? "w-[80%]" : "w-full"
+            )} 
+          />
+        );
+      })}
     </div>
   )
 }
